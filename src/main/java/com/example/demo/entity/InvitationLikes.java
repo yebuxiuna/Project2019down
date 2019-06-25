9c8da9d7740457b33b51ffc1bb55bc34cfaac934
xwx <1287212274@qq.com>
2019-06-25 15:16:45 +0800

    创建帖子的数据库操作类

--- src/main/java/com/example/demo/entity/InvitationLikes.java
+++ src/main/java/com/example/demo/entity/InvitationLikes.java
@@ -1,9 +1,13 @@
 package com.example.demo.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Entity
+@DynamicUpdate
 @Data
 public class InvitationLikes {
     @Id
